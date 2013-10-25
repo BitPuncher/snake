409d6e85d41f2047b0f242f571d641e498fde0b2
App Academy Student <student@appacademy.io>
2013-10-24 18:06:53 -0700

    Optimate draw function

--- board.js
+++ board.js
@@ -7,11 +7,10 @@
     this.width = width;
   }
 
-  var newTile
+  // var newTile
 
   var render = Board.prototype.render = function() {
     var boardState = [];
-
     for (var i = 0; i < this.snake.segments.length; i++) {
       boardState.push(["S", this.snake.segments[i]]);
     }
--- snake_ui.js
+++ snake_ui.js
@@ -4,17 +4,14 @@
   var UI = SnakeGame.UI = function (gameContainer) {
     this.container = gameContainer;
     this.game = new SnakeGame.Game();
-    this.lastRender = this.game.render();
-    this.thisRender = null;
+    this.lastRender = this.game.board.render();
   }
 
-  var initUI = UI.prototype.initUI = function (string) {
-    this.container.empty();
-    var width = this.game.board.width
-    for(var i = 0; i < string.length / width; i++) {
+  var initUI = UI.prototype.initUI = function () {
+    for (var i = 0; i < this.game.board.height; i++) {
       var row = $("<div class='row'></div>");
-      for (var j = 0; j < width; j++) {
-        row.append($("<div class='cell'></div>"));
+      for (var j = 0; j < this.game.board.width; j++) {
+        row.append($("<div class='E'></div>"));
       }
       this.container.append(row);
     }
@@ -22,11 +19,41 @@
   }
 
   var draw = UI.prototype.draw = function () {
-    a) compare lastRender to new Render
-    b) revert things that are no longer the same
-    c) change things that are new
+    var thisRender = this.game.board.render();
 
 
+    for (var i = 0; i < thisRender.length; i++){
+      for (var j = 0; j < this.lastRender.length; j++) {
+        if (thisRender[i] && this.lastRender[j] && thisRender[i][1].equals(this.lastRender[j][1])) {
+          thisRender[i] = null;
+          this.lastRender[j] = null;
+        }
+      }
+    }
+
+    for (var i = 0; i < thisRender.length; i++) {
+      row = thisRender[i][1].yPos;
+      col = thisRender[i][1].xPos;
+      className = thisRender[i][0];
+      cell = this.container.children().eq(row).children().eq(col);
+
+      console.log(cell);
+
+      cell.toggleClass(className, true);
+    }
+
+    for (var i = 0; i < this.lastRender.length; i++) {
+      row = this.lastRender[i][1].yPos;
+      col = this.lastRender[i][1].xPos;
+      className = this.lastRender[i][0];
+      cell = this.container.children().eq(row).children().eq(col);
+
+      console.log(cell);
+
+      cell.toggleClass(className, false);
+    }
+
+    this.lastRender = thisRender;
   }
 
   var bindKeys = UI.prototype.bindKeys = function () {
@@ -40,9 +67,10 @@
   var startGame = UI.prototype.startGame = function() {
     var that = this;
     this.bindKeys();
+    this.initUI();
     this.timer = window.setInterval(function () {
       that.game.step();
-      that.draw(that.game.board.render());
+      that.draw();
     }, (1000/15))
   }
 
